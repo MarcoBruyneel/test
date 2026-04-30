02866e492da7a1fefe546ec3acaa2ee19dbdf008
MarcoBruyneel <161753554+MarcoBruyneel@users.noreply.github.com>
2026-04-30 10:21:26 +0200

    Update Program.cs

--- MyProject/MyProject/Program.cs
+++ MyProject/MyProject/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("My Project");
+            // Update
 
             Console.WriteLine("Test branch!!!");
         }
